942d61f2fc605072d814597826746761d94f1bde
dlleyda <57436913+dlleyda@users.noreply.github.com>
2022-05-15 16:40:42 +0200

    pruebas_onset

--- utils/utils.py
+++ utils/utils.py
@@ -50,20 +50,55 @@ def generate_spectrogram(file_route):
         plt.savefig("./nsynth-test/MelSpectrograms/" + dato + ".png", bbox_inches='tight')
         plt.close()
 
+        
+def ver_onSet(audio):
+    y, sr = librosa.load(audio)
+    array_tiempos = librosa.onset.onset_detect(y=y, sr=sr, units='time')
+    return array_tiempos
+
 #audio_path: ruta al archivo de audio en .wav
 #save_path: directorio en el que se guardan las muestras
 #window_duration: salto de tiempo en segundos
 def separar(audio_path, save_path, window_duration):
     waveform, sample_rate = torchaudio.load(audio_path)
-    waveform = waveform.to('cuda:0')
+#     waveform = waveform.to('cuda:0')
     dimension = waveform.size(dim=1)
+    
+    array_tiempos = ver_onSet(audio_path)
+#     tensor_tiempos = torch.from_numpy(array_tiempos)
+#     tensor_tiempos = torch.tensor(array_tiempos)
 
-    anterior = 1
+#     for anterior in array_tiempos:
+#     i = 0
+#     for anterior_float in array_tiempos:
+# #         anterior_float = anterior_tensor.item()
+#         anterior = int(anterior_float)*sample_rate
+#         print(anterior)
+#         audio = torch.narrow(waveform, dim=1, start = anterior, length = sample_rate*4)
+#         audio = SignalTransformation.generarSpectrogramaFromSignal(audio)
+#         shape = audio.shape
+#         audio = torchaudio.transforms.AmplitudeToDB()(audio)
+#         audio=audio.cpu().data.numpy()
+#         librosa.display.specshow(audio[0],cmap='magma')
+#         nombre = save_path+str(i)+".png"
+#         try:
+#             plt.savefig(nombre, bbox_inches='tight')
+#             plt.close()
+#         except:
+#             if not os.path.exists(save_path):
+#                 os.makedirs(save_path)
+#                 plt.savefig(nombre, bbox_inches='tight')
+#                 plt.close()
+#         i = i+1
+#         print(anterior.item())
+
+#     anterior = 1
+    anterior = int(array_tiempos[0])*sample_rate
     i=0
-    k=window_duration * sample_rate
+#     k=window_duration * sample_rate
     while (anterior+sample_rate*4) <= dimension:
             audio = torch.narrow(waveform, dim=1, start = anterior, length = sample_rate*4)
-            anterior = anterior + k
+#             anterior = anterior + k
             #print(prueba.size(dim=1))
 
             audio = SignalTransformation.generarSpectrogramaFromSignal(audio)
@@ -75,6 +110,7 @@ def separar(audio_path, save_path, window_duration):
             plt.savefig(nombre, bbox_inches='tight')
             plt.close()
             i = i+1
+            anterior = int(array_tiempos[i])*sample_rate
 
 
 
